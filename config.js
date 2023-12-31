f0f0cc213e507c7ae5d515cc1c9b9b8e6d887c12
Travisdx <150427515+Travisdx@users.noreply.github.com>
2023-12-31 02:13:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349066380818', 'TRAVIS', true], 
   [''] 
 ] //Number of owners
 
@@ -53,7 +52,7 @@ global.APIKeys = { // APIKey Here
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.author = '@TRAVIS' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
